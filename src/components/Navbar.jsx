782a9518943289274af59cc4aa30ef75f3800648
“Nilde <nilde.benedetti@gmail.com>
2026-05-14 17:04:24 +0200

    add: formattazione navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,34 @@
 import { Link, NavLink } from "react-router";
 
 function Navbar() {
-    return <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid">
-            <Link to="/" className="navbar-brand" >
-                <img src="./images/lastdrop-logo-1.png" alt="lastdrop logo" className="navbar-logo"></img>
-            </Link>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-                <ul className="navbar-nav">
-                    <li className="nav-item">
-                        <NavLink to="/" className="nav-link active">Home</NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink to="/prodotti" className="nav-link">Prodotti</NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink to="/chi-siamo" className="nav-link">Chi Siamo</NavLink>
-                    </li>
-                </ul>
+    return <nav className="navbar navbar-expand-lg bg-body-tertiary d-flex">
+        <div className="container-fluid d-flex justify-content-between">
+            <div className="left-navbar d-flex">
+                <Link to="/" className="navbar-brand" >
+                    <img src="./images/lastdrop-logo-1.png" alt="lastdrop logo" className="navbar-logo"></img>
+                </Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <NavLink to="/" className="nav-link active">Home</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink to="/prodotti" className="nav-link">Prodotti</NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink to="/chi-siamo" className="nav-link">Chi Siamo</NavLink>
+                        </li>
+                    </ul>
+                </div>
             </div>
-            <form className="container-fluid justify-content-start">
-                <button className="btn btn-sm btn-outline-warning" type="button">Modalità Povero</button>
-            </form>
+            <div className="right-sidebar">
+                <form className="container-fluid justify-content-start">
+                    <button className="btn btn-sm btn-outline-warning" type="button">Modalità Povero</button>
+                </form>
+            </div>
         </div>
     </nav>
 }
